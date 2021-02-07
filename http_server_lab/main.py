c24b0d2bbb5e155014d68cbb54dcb2cd9623cce6
vadbeg <vadbeg@tut.by>
2021-02-07 15:40:02 +0300

    Fixed imports problem

--- http_server_lab/main.py
+++ http_server_lab/main.py
@@ -1,9 +1,28 @@
 """Server starting script"""
 
+import sys
+sys.path.append('/home/vadbeg/Projects/University/networks-and-info-security')
+
+import argparse
 
 from http_server_lab.servers.http_server import HTTPServer
 
 
+def get_args():
+    argument_parser = argparse.ArgumentParser(description=f'Script for server setup')
+
+    argument_parser.add_argument('--port', help='Server port', type=int, default=8888)
+    argument_parser.add_argument('--host', help='Server host', type=str, default='localhost')
+    argument_parser.add_argument('-df', '--data-folder', help='Folder with all data', type=str, default='data')
+
+    args = argument_parser.parse_args()
+
+    return args
+
+
 if __name__ == '__main__':
-    server = HTTPServer()
+    args = get_args()
+
+    server = HTTPServer(port=args.port, host=args.host, data_folder=args.data_folder)
+
     server.start()
